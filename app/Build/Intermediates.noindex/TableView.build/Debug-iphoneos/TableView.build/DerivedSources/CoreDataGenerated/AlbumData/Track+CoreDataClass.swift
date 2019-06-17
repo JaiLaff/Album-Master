29c5d04901a9165375f05c433b3003099fb28953
Jai Lafferty <Jai.lafferty@gmail.com>
2019-06-17 17:38:41 +1000

    Final Commit

--- app/Build/Intermediates.noindex/TableView.build/Debug-iphoneos/TableView.build/DerivedSources/CoreDataGenerated/AlbumData/Track+CoreDataClass.swift
+++ app/Build/Intermediates.noindex/TableView.build/Debug-iphoneos/TableView.build/DerivedSources/CoreDataGenerated/AlbumData/Track+CoreDataClass.swift
@@ -2,7 +2,7 @@
 //  Track+CoreDataClass.swift
 //  
 //
-//  Created by Jai Lafferty on 12/6/19.
+//  Created by Jai Lafferty on 14/6/19.
 //
 //  This file was automatically generated and should not be edited.
 //
